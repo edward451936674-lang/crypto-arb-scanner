403f8a3ad42a9459eff7c84c245709de7901e9f6
edward451936674-lang <edward451936674@gmail.com>
2026-04-12 10:46:09 +0800

    Add non-live venue execution adapter stubs and preview API

--- app/execution_adapters/__init__.py
+++ app/execution_adapters/__init__.py
@@ -1,4 +1,20 @@
 from app.execution_adapters.base import BaseExecutionAdapter
 from app.execution_adapters.paper import PaperExecutionAdapter
+from app.execution_adapters.registry import ADAPTER_REGISTRY, get_execution_adapter
+from app.execution_adapters.stubs import (
+    BinanceExecutionAdapterStub,
+    HyperliquidExecutionAdapterStub,
+    LighterExecutionAdapterStub,
+    OkxExecutionAdapterStub,
+)
 
-__all__ = ["BaseExecutionAdapter", "PaperExecutionAdapter"]
+__all__ = [
+    "BaseExecutionAdapter",
+    "PaperExecutionAdapter",
+    "BinanceExecutionAdapterStub",
+    "OkxExecutionAdapterStub",
+    "HyperliquidExecutionAdapterStub",
+    "LighterExecutionAdapterStub",
+    "ADAPTER_REGISTRY",
+    "get_execution_adapter",
+]
